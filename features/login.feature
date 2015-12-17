1cc356c7953bd77927706edb429abda5f9dfe489
Joel Efah <JEfah@spartaglobal.com>
2015-12-17 14:35:45 +0000

    added new negative test

--- features/login.feature
+++ features/login.feature
@@ -1,10 +1,15 @@
 Feature: Login to Tumblr  
-  
+
+Background:
+  Given I am on the login page
+
+   Scenario: incorrect login credentials
+    When I enter the incorrect credentials
+    Then I should remain in the login page
+
   Scenario: correct login credentials
-    Given I am on the login page
     When I enter the correct credentials
     Then I should be on the dash board
 
-  
-
+ 
   
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -4,6 +4,17 @@ Given(/^I am on the login page$/) do
   @b.visit
 end
 
+When(/^I enter the incorrect credentials$/) do
+  @b.email "incorrect@gmail.com"
+  @b.password "incorrect123"
+  @b.login_button
+end
+
+Then(/^I should remain in the login page$/) do
+  expect(@b.url).to eq "https://www.tumblr.com/login"
+end
+
+
 When(/^I enter the correct credentials$/) do
   @b.email "300globaltest@gmail.com"
   @b.password "TESTing123"
@@ -13,3 +24,4 @@ end
 Then(/^I should be on the dash board$/) do
   expect(@b.url).to eq "https://www.tumblr.com/dashboard"
 end
+
